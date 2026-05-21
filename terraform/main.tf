1c4e1e266b16edf359e7218b8498cc6cccd2fea0
Riahi Semah <riahisamh817@gmail.com>
2026-05-21 22:06:25 +0100

    fix pipline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,21 @@
 # ============================================================
+# AMI Data Source for Amazon Linux 2023
+# ============================================================
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+# ============================================================
 # Provider
 # ============================================================
 provider "aws" {
